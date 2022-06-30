cdab1023b868631624ce250db1ae4c8379a3e809
corno <corno.astn.a@schraverus.com>
2022-06-30 14:26:09 +0200

    .

--- pareto/scripts/buildAndTest.sh
+++ pareto/scripts/buildAndTest.sh
@@ -3,10 +3,15 @@
 if [ -d "../dev" ]
 then
     "$(dirname "$0")"/buildDevPackage.sh && \
-    rm -rf ../pub/src/generated && \
-    node ../dev/dist/esc/bin/generateCode.js ../pub/src/generated
+    rm -rf ../api/src/generated && \
+    rm -rf ../lib/src/generated && \
+    node ../dev/dist/esc/bin/generateCode.js ..
 fi \
 
 
 "$(dirname "$0")"/buildPubAndTestPackages.sh && \
-node ../test/dist/esc/bin/index.js ../test/data
+if [ -d "../test" ]
+then
+    node ../test/dist/esc/bin/index.js ../test/data
+fi \
+
--- pareto/scripts/publish.sh
+++ pareto/scripts/publish.sh
@@ -4,14 +4,11 @@ dir=`realpath $(dirname "$0")`
 #make sure everything is pushed
 git push && \
 
-#validate that everything is committed (to make sure we're not messing with open work with updatePackage)
-git diff --exit-code && \
+#validate that everything is committed and pushed (to make sure we're not messing with open work with updatePackage)
+git diff --exit-code && git log origin/master..master --exit-code && \
 
 #make sure latest packages are installed
-$dir/updatePackage.sh ../pub && \
-
-#validate that everything is committed
-git diff --exit-code && \
+$dir/updatePackage.sh ../$2 && \
 
 #buildAndTest
 $dir/buildAndTest.sh && \
@@ -21,7 +18,7 @@ $dir/buildAndTest.sh && \
 #####$dir/analyseAllTypeScriptProjects.sh && \
 
 #bump version and store in variable
-pushd ../pub && \
+pushd ../$2 > /dev/null && \
 newVersion=$(npm version "$1") && \
 popd && \
 
@@ -34,6 +31,6 @@ git tag -a "$newVersion" -m "$newVersion" && \
 git push && \
 
 #publish
-pushd ../pub && \
+pushd ../$2 > /dev/null && \
 npm publish && \
 popd
