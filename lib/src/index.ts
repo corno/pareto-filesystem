bd78087931c9c3e67e92f0ba280d0ebf6c9fabff
corno <corno.astn.a@schraverus.com>
2022-07-04 14:45:10 +0200

    .

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -5,7 +5,6 @@ import { directory, file } from "./asyncAPI"
 export * from "./esc" //FIXME!!! REMOVE
 export * from "./interface" //FIXME!!! REMOVE
 
-
 export function init(
 ): API {
     return {
