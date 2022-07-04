7b8aef00e3551efdd5e6bbb0121b08bea0b681f3
corno <corno.astn.a@schraverus.com>
2022-07-04 11:09:34 +0200

    .

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,16 +1,15 @@
 
 import { API } from "pareto-filesystem-api"
 import { directory, file } from "./asyncAPI"
-import * as asyncAPI from "pareto-async-api"
 
 export * from "./esc" //FIXME!!! REMOVE
 export * from "./interface" //FIXME!!! REMOVE
 
 
-// export function init(
-// ): API {
-//     return {
-//         file: file,
-//         directory: directory(),
-//     }
-// }
+export function init(
+): API {
+    return {
+        file: file,
+        directory: directory(),
+    }
+}
